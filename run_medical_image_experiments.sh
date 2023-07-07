1879a1b57ba23690723b8d216959505f1ff37c21
AKA Math <amithjkamath@users.noreply.github.com>
2023-07-08 00:24:36 +0200

    update scripts

---
+++ run_medical_image_experiments.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python ./toskipornot/train_glas_2d_full.py
+python ./toskipornot/train_busi_2d_full.py
+python ./toskipornot/train_spleen_2d_full.py
+python ./toskipornot/train_heart_2d_full.py
